b1f4db86085cbc59d0a66e49f8233f3d70f6fc5a
Vee Uye <vee.uye@gmail.com>
2022-02-27 17:26:59 +0000

    solved reachDestination

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,14 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual(
+      `I should be there in ${4.5} hours.`
+    );
+    expect(reachDestination(15, 3)).toEqual(`I should be there in ${5} hours.`);
+
+    expect(reachDestination(77, 30)).toEqual(
+      `I should be there in ${2.5} hours.`
+    );
+  });
 });
